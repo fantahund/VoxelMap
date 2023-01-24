36ac351e64dab5135d1724982e68baeb0f8d219f
TheAlgorithm476 <thealgorithm476@gmail.com>
2023-01-24 19:40:30 +0100

    Backport changes from 23w04a

--- src/main/java/com/mamiyaotaru/voxelmap/fabricmod/Share.java
+++
@@ -1,9 +0,0 @@
-package com.mamiyaotaru.voxelmap.fabricmod;
-
-import com.mamiyaotaru.voxelmap.VoxelConstants;
-
-public final class Share {
-    private Share() {}
-
-    public static boolean isOldNorth() { return VoxelConstants.getVoxelMapInstance().getMapOptions().oldNorth; }
-}
