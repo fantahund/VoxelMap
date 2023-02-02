0bda7e3444633426d855a979c1a4f77f3b45c749
TheAlgorithm476 <thealgorithm476@gmail.com>
2023-02-02 21:41:16 +0100

    Optimize TextUtils

--- src/main/java/com/mamiyaotaru/voxelmap/util/TextUtils.java
+++ src/main/java/com/mamiyaotaru/voxelmap/util/TextUtils.java
@@ -3,93 +3,117 @@ package com.mamiyaotaru.voxelmap.util;
 import net.minecraft.text.Style;
 import net.minecraft.text.Text;
 import net.minecraft.util.Formatting;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
-public class TextUtils {
-    public static String scrubCodes(String string) {
-        return string.replaceAll("(§.)", "");
-    }
+public final class TextUtils {
+    private static final Pattern CODE_SCRUBBING_PATTERN = Pattern.compile("(§.)");
 
+    private TextUtils() {}
+
+    public static String scrubCodes(String string) { return CODE_SCRUBBING_PATTERN.matcher(string).replaceAll(""); }
+
+    @NotNull
     public static String scrubName(String input) {
-        input = input.replace(",", "~comma~");
-        return input.replace(":", "~colon~");
+        if (input == null) return "";
+
+        return input
+                .replace(",", "~comma~")
+                .replace(":", "~colon~");
     }
 
+    @NotNull
     public static String scrubNameRegex(String input) {
-        input = input.replace(",", "﹐");
-        input = input.replace("[", "⟦");
-        return input.replace("]", "⟧");
+        if (input == null) return "";
+
+        return input
+                .replace(',', '﹐')
+                .replace('[', '⟦')
+                .replace(']', '⟧');
     }
 
+    @NotNull
     public static String scrubNameFile(String input) {
-        input = input.replace("<", "~less~");
-        input = input.replace(">", "~greater~");
-        input = input.replace(":", "~colon~");
-        input = input.replace("\"", "~quote~");
-        input = input.replace("/", "~slash~");
-        input = input.replace("\\", "~backslash~");
-        input = input.replace("|", "~pipe~");
-        input = input.replace("?", "~question~");
-        return input.replace("*", "~star~");
+        if (input == null) return "";
+
+        return input
+                .replace("<", "~less~")
+                .replace(">", "~greater~")
+                .replace(":", "~colon~")
+                .replace("\"", "~quote~")
+                .replace("/", "~slash~")
+                .replace("\\", "~backslash~")
+                .replace("|", "~pipe~")
+                .replace("?", "~question~")
+                .replace("*", "~star~");
     }
 
+    @NotNull
     public static String descrubName(String input) {
-        input = input.replace("~less~", "<");
-        input = input.replace("~greater~", ">");
-        input = input.replace("~colon~", ":");
-        input = input.replace("~quote~", "\"");
-        input = input.replace("~slash~", "/");
-        input = input.replace("~backslash~", "\\");
-        input = input.replace("~pipe~", "|");
-        input = input.replace("~question~", "?");
-        input = input.replace("~star~", "*");
-        input = input.replace("~comma~", ",");
-        input = input.replace("~colon~", ":");
-        input = input.replace("﹐", ",");
-        input = input.replace("⟦", "[");
-        return input.replace("⟧", "]");
+        if (input == null) return "";
+
+        return input
+                .replace("~less~", "<")
+                .replace("~greater~", ">")
+                .replace("~colon~", ":")
+                .replace("~quote~", "\"")
+                .replace("~slash~", "/")
+                .replace("~backslash~", "\\")
+                .replace("~pipe~", "|")
+                .replace("~question~", "?")
+                .replace("~star~", "*")
+                .replace("~comma~", ",")
+                .replace("~colon~", ":")
+                .replace('﹐', ',')
+                .replace('⟦', '[')
+                .replace('⟧', ']');
     }
 
+    @NotNull
     public static String prettify(String input) {
-        String[] words = input.split("_");
+        if (input == null) return "";
 
-        for (int t = 0; t < words.length; ++t) {
-            words[t] = words[t].substring(0, 1).toUpperCase() + words[t].substring(1).toLowerCase();
-        }
-
-        return String.join(" ", words);
+        return Arrays
+                .stream(input.split("_"))
+                .map(word -> word.substring(0, 1).toUpperCase() + word.substring(1).toLowerCase())
+                .collect(Collectors.joining(" "));
     }
 
+    @NotNull
     public static String asFormattedString(Text text2) {
         StringBuilder stringBuilder = new StringBuilder();
         String lastStyleString = "";
 
-        for (Text text : TextUtils.stream(text2)) {
+        for (Text text : stream(text2)) {
             String contentString = text.getString();
-            if (!contentString.isEmpty()) {
-                String styleString = asString(text.getStyle());
-                if (!styleString.equals(lastStyleString)) {
-                    if (!lastStyleString.isEmpty()) {
-                        stringBuilder.append(Formatting.RESET);
-                    }
 
-                    stringBuilder.append(styleString);
-                    lastStyleString = styleString;
-                }
+            if (contentString.isEmpty()) continue;
 
-                stringBuilder.append(contentString);
+            String styleString = asString(text.getStyle());
+
+            if (styleString.equals(lastStyleString)) {
+                stringBuilder.append(styleString);
+                continue;
             }
+
+            if (!(lastStyleString.isEmpty())) stringBuilder.append(Formatting.RESET);
+
+            stringBuilder.append(styleString);
+            lastStyleString = styleString;
+
+            stringBuilder.append(contentString);
         }
 
-        if (!lastStyleString.isEmpty()) {
-            stringBuilder.append(Formatting.RESET);
-        }
-
+        if (!(lastStyleString.isEmpty())) stringBuilder.append(Formatting.RESET);
         return stringBuilder.toString();
     }
 
+    @NotNull
     private static List<Text> stream(Text text) {
         List<Text> stream = new ArrayList<>();
         stream.add(text);
@@ -101,39 +125,23 @@ public class TextUtils {
         return stream;
     }
 
+    @NotNull
     private static String asString(Style style) {
-        if (style.isEmpty()) {
-            return "";
-        } else {
-            StringBuilder stringBuilder = new StringBuilder();
-            if (style.getColor() != null) {
-                Formatting colorFormat = Formatting.byName(style.getColor().getName());
-                if (colorFormat != null) {
-                    stringBuilder.append(colorFormat);
-                }
-            }
+        if (style == null || style.isEmpty()) return "";
 
-            if (style.isBold()) {
-                stringBuilder.append(Formatting.BOLD);
-            }
+        StringBuilder stringBuilder = new StringBuilder();
 
-            if (style.isItalic()) {
-                stringBuilder.append(Formatting.ITALIC);
-            }
+        if (style.getColor() != null) {
+            Formatting colorFormat = Formatting.byName(style.getColor().getName());
 
-            if (style.isUnderlined()) {
-                stringBuilder.append(Formatting.UNDERLINE);
-            }
-
-            if (style.isObfuscated()) {
-                stringBuilder.append(Formatting.OBFUSCATED);
-            }
-
-            if (style.isStrikethrough()) {
-                stringBuilder.append(Formatting.STRIKETHROUGH);
-            }
-
-            return stringBuilder.toString();
+            if (colorFormat != null) stringBuilder.append(colorFormat);
         }
+
+        if (style.isBold()) stringBuilder.append(Formatting.BOLD);
+        if (style.isItalic()) stringBuilder.append(Formatting.ITALIC);
+        if (style.isUnderlined()) stringBuilder.append(Formatting.UNDERLINE);
+        if (style.isObfuscated()) stringBuilder.append(Formatting.OBFUSCATED);
+        if (style.isStrikethrough()) stringBuilder.append(Formatting.STRIKETHROUGH);
+        return stringBuilder.toString();
     }
 }
