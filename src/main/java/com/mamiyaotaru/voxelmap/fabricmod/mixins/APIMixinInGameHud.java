076f4d74aad9519e47188f21d528124ed3653e46
fanta <tobiasmuellner@outlook.com>
2024-06-15 05:10:46 +0200

    redner map in main hud

--- src/main/java/com/mamiyaotaru/voxelmap/fabricmod/mixins/APIMixinInGameHud.java
+++ src/main/java/com/mamiyaotaru/voxelmap/fabricmod/mixins/APIMixinInGameHud.java
@@ -11,7 +11,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(InGameHud.class)
 public class APIMixinInGameHud {
-    @Inject(method = "render", at = @At("RETURN"))
+    @Inject(method = "renderMainHud", at = @At("RETURN"))
     private void onRenderGameOverlay(DrawContext context, RenderTickCounter tickCounter, CallbackInfo ci) {
         FabricModVoxelMap.instance.renderOverlay(context);
     }
