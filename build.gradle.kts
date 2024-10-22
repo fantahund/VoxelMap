be0a92d564b588cfa046cf2bbe59ea707bb90820
fanta <tobiasmuellner@outlook.com>
2024-10-22 03:28:37 +0200

    1.21.2 rc2

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
     id("fabric-loom") version ("1.8-SNAPSHOT") apply (false)
 }
 
-val MINECRAFT_VERSION by extra { "1.21.2-rc1" }
+val MINECRAFT_VERSION by extra { "1.21.2-rc2" }
 val NEOFORGE_VERSION by extra { "21.2.0-alpha.1.21.2-rc1.20241020.190700" }
 val FABRIC_LOADER_VERSION by extra { "0.16.7" }
 val FABRIC_API_VERSION by extra { "0.106.0+1.21.2" }
