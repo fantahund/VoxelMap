924922116b1604dfc416378ef46749553fc917a3
Brokkonaut <hannos17@gmx.de>
2024-11-28 09:31:26 +0100

    Fix icon in GuiSlotDimensions + cleanup

--- common/src/main/java/com/mamiyaotaru/voxelmap/gui/GuiSlotDimensions.java
+++ common/src/main/java/com/mamiyaotaru/voxelmap/gui/GuiSlotDimensions.java
@@ -14,10 +14,13 @@ import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 
 class GuiSlotDimensions extends GuiSlotMinimap {
-    final GuiAddWaypoint parentGui;
+    private static final Component APPLIES = Component.translatable("minimap.waypoints.dimension.applies");
+    private static final Component NOT_APPLIES = Component.translatable("minimap.waypoints.dimension.notapplies");
+    private static final ResourceLocation CONFIRM = ResourceLocation.parse("textures/gui/sprites/container/beacon/confirm.png");
+    private static final ResourceLocation CANCEL = ResourceLocation.parse("textures/gui/sprites/container/beacon/cancel.png");
+
+    private final GuiAddWaypoint parentGui;
     private final ArrayList<DimensionItem> dimensions;
-    static final Component APPLIES = Component.translatable("minimap.waypoints.dimension.applies");
-    static final Component NOT_APPLIES = Component.translatable("minimap.waypoints.dimension.notapplies");
 
     GuiSlotDimensions(GuiAddWaypoint par1GuiWaypoints) {
         super(101, par1GuiWaypoints.getHeight(), par1GuiWaypoints.getHeight() / 6 + 82 + 6, par1GuiWaypoints.getHeight() / 6 + 164 + 3, 18);
@@ -71,7 +74,7 @@ class GuiSlotDimensions extends GuiSlotMinimap {
         public void render(GuiGraphics drawContext, int index, int y, int x, int entryWidth, int entryHeight, int mouseX, int mouseY, boolean hovered, float tickDelta) {
             drawContext.drawCenteredString(this.parentGui.getFontRenderer(), this.dim.getDisplayName(), this.parentGui.getWidth() / 2 + GuiSlotDimensions.this.slotWidth / 2, y + 3, 16777215);
             byte padding = 4;
-            byte iconWidth = 16;
+            byte iconWidth = 18;
             x = this.parentGui.getWidth() / 2;
             int width = GuiSlotDimensions.this.slotWidth;
             if (mouseX >= x + padding && mouseY >= y && mouseX <= x + width + padding && mouseY <= y + GuiSlotDimensions.this.itemHeight) {
@@ -85,11 +88,13 @@ class GuiSlotDimensions extends GuiSlotMinimap {
                 GuiAddWaypoint.setTooltip(this.parentGui, tooltip);
             }
 
+            // show check mark / cross
             OpenGL.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
-            OpenGL.Utils.img2("textures/gui/container/beacon.png");
-            int xOffset = this.parentGui.waypoint.dimensions.contains(this.dim) ? 91 : 113;
-            int yOffset = 222;
-            drawContext.blit(RenderType::guiTextured, ResourceLocation.parse("textures/gui/container/beacon.png"), x + width - iconWidth, y - 2, xOffset, yOffset, 16, 16, 256, 256);
+            // 2 int: x,y screen
+            // 2 float: u,v start texture (in pixels - see last 2 int)
+            // 2 int: height, width on screen
+            // 2 int: height, width full texture in pixels
+            drawContext.blit(RenderType::guiTextured, this.parentGui.waypoint.dimensions.contains(this.dim) ? CONFIRM : CANCEL, x + width - iconWidth, y - 3, 0, 0, 18, 18, 18, 18);
             drawContext.flush();
         }
 
@@ -97,7 +102,7 @@ class GuiSlotDimensions extends GuiSlotMinimap {
             GuiSlotDimensions.this.setSelected(this);
             int leftEdge = this.parentGui.getWidth() / 2;
             byte padding = 4;
-            byte iconWidth = 16;
+            byte iconWidth = 18;
             int width = GuiSlotDimensions.this.slotWidth;
             if (mouseX >= (leftEdge + width - iconWidth - padding) && mouseX <= (leftEdge + width)) {
                 this.parentGui.toggleDimensionSelected();
