5e751599a5de69d6747156d222daea464fa8d554
fantahund <tobiasmuellner@outlook.com>
2025-04-28 05:59:54 +0200

    Update Dependencies (Version++)

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,11 +5,11 @@ plugins {
 
 val MINECRAFT_VERSION by extra { "1.21.5" }
 val NEOFORGE_VERSION by extra { "21.5.47-beta" }
-val FABRIC_LOADER_VERSION by extra { "0.16.10" }
-val FABRIC_API_VERSION by extra { "0.119.5+1.21.5" }
+val FABRIC_LOADER_VERSION by extra { "0.16.14" }
+val FABRIC_API_VERSION by extra { "0.121.0+1.21.5" }
 val IRIS_VERSION by extra { "1.8.11" }
 
-val MOD_VERSION by extra { "$MINECRAFT_VERSION-1.15.2" }
+val MOD_VERSION by extra { "$MINECRAFT_VERSION-1.15.3" }
 
 allprojects {
     apply(plugin = "java")
