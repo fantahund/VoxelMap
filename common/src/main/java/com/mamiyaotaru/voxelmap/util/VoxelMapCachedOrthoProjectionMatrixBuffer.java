371a05bb5bbefa298cc4656db8f916cc6e3c4f3e
Brokkonaut <hannos17@gmx.de>
2025-06-06 07:31:35 +0200

    ja ups

--- common/src/main/java/com/mamiyaotaru/voxelmap/util/VoxelMapCachedOrthoProjectionMatrixBuffer.java
+++ common/src/main/java/com/mamiyaotaru/voxelmap/util/VoxelMapCachedOrthoProjectionMatrixBuffer.java
@@ -15,28 +15,28 @@ import com.mojang.blaze3d.systems.RenderSystem;
  * See {@link net.minecraft.client.renderer.CachedOrthoProjectionMatrixBuffer}
  */
 public class VoxelMapCachedOrthoProjectionMatrixBuffer implements AutoCloseable {
-	private final GpuBuffer buffer;
-	private final GpuBufferSlice bufferSlice;
+    private final GpuBuffer buffer;
+    private final GpuBufferSlice bufferSlice;
 
-	public VoxelMapCachedOrthoProjectionMatrixBuffer(String string, float left, float right, float bottom, float top, float zNear, float zFar) {
-		GpuDevice gpuDevice = RenderSystem.getDevice();
-		this.buffer = gpuDevice.createBuffer(() -> "Projection matrix UBO " + string, 136, RenderSystem.PROJECTION_MATRIX_UBO_SIZE);
-		this.bufferSlice = this.buffer.slice(0, RenderSystem.PROJECTION_MATRIX_UBO_SIZE);
-		
-		Matrix4f matrix4f = new Matrix4f().ortho(left, right, bottom, top, zNear, zFar);
+    public VoxelMapCachedOrthoProjectionMatrixBuffer(String string, float left, float right, float bottom, float top, float zNear, float zFar) {
+        GpuDevice gpuDevice = RenderSystem.getDevice();
+        this.buffer = gpuDevice.createBuffer(() -> "Projection matrix UBO " + string, GpuBuffer.USAGE_UNIFORM + GpuBuffer.USAGE_COPY_DST, RenderSystem.PROJECTION_MATRIX_UBO_SIZE);
+        this.bufferSlice = this.buffer.slice(0, RenderSystem.PROJECTION_MATRIX_UBO_SIZE);
 
-		try (MemoryStack memoryStack = MemoryStack.stackPush()) {
-			ByteBuffer byteBuffer = Std140Builder.onStack(memoryStack, RenderSystem.PROJECTION_MATRIX_UBO_SIZE)
-					.putMat4f(matrix4f).get();
-			RenderSystem.getDevice().createCommandEncoder().writeToBuffer(this.buffer.slice(), byteBuffer);
-		}
-	}
+        Matrix4f matrix4f = new Matrix4f().ortho(left, right, bottom, top, zNear, zFar);
 
-	public GpuBufferSlice getBuffer() {
-		return this.bufferSlice;
-	}
+        try (MemoryStack memoryStack = MemoryStack.stackPush()) {
+            ByteBuffer byteBuffer = Std140Builder.onStack(memoryStack, RenderSystem.PROJECTION_MATRIX_UBO_SIZE)
+                    .putMat4f(matrix4f).get();
+            RenderSystem.getDevice().createCommandEncoder().writeToBuffer(this.buffer.slice(), byteBuffer);
+        }
+    }
 
-	public void close() {
-		this.buffer.close();
-	}
+    public GpuBufferSlice getBuffer() {
+        return this.bufferSlice;
+    }
+
+    public void close() {
+        this.buffer.close();
+    }
 }
