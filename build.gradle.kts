9d2c215efa2cf613b7863d2aa8353e8c0641036f
Brokkonaut <hannos17@gmx.de>
2025-07-02 03:36:04 +0200

    Release 1.21.7-1.15.5

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ val NEOFORGE_VERSION by extra { "21.5.47-beta" }
 val FABRIC_LOADER_VERSION by extra { "0.16.14" }
 val FABRIC_API_VERSION by extra { "0.128.1+1.21.7" }
 val IRIS_VERSION by extra { "1.9.1" }
-val VOXELMAP_VERSION by extra { "1.15.4" }
+val VOXELMAP_VERSION by extra { "1.15.5" }
 
 val MOD_VERSION by extra { "$MINECRAFT_VERSION-$VOXELMAP_VERSION" }
 
