d772abf62dae64d065b2290024db7b436a04a0ac
Brokkonaut <hannos17@gmx.de>
2025-07-17 06:34:12 +0200

    remove unused method

--- common/src/main/java/com/mamiyaotaru/voxelmap/util/GLUtils.java
+++ common/src/main/java/com/mamiyaotaru/voxelmap/util/GLUtils.java
@@ -1,6 +1,5 @@
 package com.mamiyaotaru.voxelmap.util;
 
-import com.google.common.base.Preconditions;
 import com.mojang.blaze3d.buffers.GpuBuffer;
 import com.mojang.blaze3d.systems.CommandEncoder;
 import com.mojang.blaze3d.systems.RenderSystem;
@@ -10,24 +9,6 @@ import java.util.function.Consumer;
 import net.minecraft.util.ARGB;
 
 public class GLUtils {
-    public static void readTextureContentsToPixelArray(GpuTexture gpuTexture, Consumer<int[]> resultConsumer) {
-        Preconditions.checkNotNull(resultConsumer);
-        int size = gpuTexture.getWidth(0) * gpuTexture.getHeight(0);
-        int bufferSize = gpuTexture.getFormat().pixelSize() * size;
-        GpuBuffer gpuBuffer = RenderSystem.getDevice().createBuffer(() -> "Texture read buffer", GpuBuffer.USAGE_MAP_READ | GpuBuffer.USAGE_COPY_DST, bufferSize);
-        CommandEncoder commandEncoder = RenderSystem.getDevice().createCommandEncoder();
-
-        commandEncoder.copyTextureToBuffer(gpuTexture, gpuBuffer, 0, () -> {
-            try (GpuBuffer.MappedView readView = commandEncoder.mapBuffer(gpuBuffer, true, false)) {
-                int[] pixels = new int[size];
-                readView.data().asIntBuffer().get(0, pixels);
-                resultConsumer.accept(pixels);
-            } finally {
-                gpuBuffer.close();
-            }
-        }, 0);
-    }
-
     public static void readTextureContentsToBufferedImage(GpuTexture gpuTexture, Consumer<BufferedImage> resultConsumer) {
         RenderSystem.assertOnRenderThread();
         int bytePerPixel = gpuTexture.getFormat().pixelSize();
