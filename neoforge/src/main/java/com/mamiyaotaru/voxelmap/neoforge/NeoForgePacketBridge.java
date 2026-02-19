e657546023d93cbe375c384140f0007200b21997
Gamja <algamja2011@gmail.com>
2026-02-19 17:32:15 +0900

    fix neoforge packet error

--- neoforge/src/main/java/com/mamiyaotaru/voxelmap/neoforge/NeoForgePacketBridge.java
+++ neoforge/src/main/java/com/mamiyaotaru/voxelmap/neoforge/NeoForgePacketBridge.java
@@ -1,12 +1,17 @@
 package com.mamiyaotaru.voxelmap.neoforge;
 
 import com.mamiyaotaru.voxelmap.PacketBridge;
-import com.mamiyaotaru.voxelmap.packets.WorldIdS2C;
+import com.mamiyaotaru.voxelmap.packets.WorldIdC2S;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.multiplayer.ClientPacketListener;
 import net.neoforged.neoforge.client.network.ClientPacketDistributor;
 
 public class NeoForgePacketBridge implements PacketBridge {
     @Override
     public void sendWorldIDPacket() {
-        ClientPacketDistributor.sendToServer(new WorldIdS2C(""));
+        ClientPacketListener connection = Minecraft.getInstance().getConnection();
+        if (connection != null && connection.hasChannel(WorldIdC2S.PACKET_ID.id())){
+            ClientPacketDistributor.sendToServer(new WorldIdC2S());
+        }
     }
 }
