40b195a0f7ea5b84239bee3c85ee6a720df5170e
Tobi <tobiasmuellner@outlook.com>
2026-03-05 16:16:44 +0100

    fic forge packet sending

--- forge/src/main/java/com/mamiyaotaru/voxelmap/forge/ForgePacketBridge.java
+++ forge/src/main/java/com/mamiyaotaru/voxelmap/forge/ForgePacketBridge.java
@@ -1,12 +1,9 @@
 package com.mamiyaotaru.voxelmap.forge;
 
 import com.mamiyaotaru.voxelmap.PacketBridge;
-import com.mamiyaotaru.voxelmap.packets.WorldIdC2S;
 import com.mamiyaotaru.voxelmap.packets.WorldIdS2C;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.multiplayer.ClientPacketListener;
-import net.minecraft.network.protocol.Packet;
-import net.minecraft.network.protocol.common.ServerboundCustomPayloadPacket;
 import net.minecraftforge.network.PacketDistributor;
 
 public class ForgePacketBridge implements PacketBridge {
@@ -14,8 +11,7 @@ public class ForgePacketBridge implements PacketBridge {
     public void sendWorldIDPacket() {
         ClientPacketListener connection = Minecraft.getInstance().getConnection();
         if (connection != null && ForgeWorldIdPacketHandler.WORLD_ID.isRemotePresent(connection.getConnection())) {
-            // ohh stop crashing please
-//            ForgeWorldIdPacketHandler.WORLD_ID.send(new WorldIdC2S(), PacketDistributor.SERVER.noArg());
+            ForgeWorldIdPacketHandler.WORLD_ID.send(new WorldIdS2C(""), PacketDistributor.SERVER.noArg());
         }
     }
 }
