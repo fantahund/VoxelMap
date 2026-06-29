499afef5d36247b5d76f85fec93832c31991c756
Brokkonaut <hannos17@gmx.de>
2026-06-29 06:30:09 +0200

    fix worldid packet

--- common/src/main/java/com/mamiyaotaru/voxelmap/packets/WorldIdPayload.java
+++ common/src/main/java/com/mamiyaotaru/voxelmap/packets/WorldIdPayload.java
@@ -1,23 +1,46 @@
 package com.mamiyaotaru.voxelmap.packets;
 
 import com.mamiyaotaru.voxelmap.VoxelConstants;
+import java.nio.charset.StandardCharsets;
 import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.network.codec.StreamCodec;
 import net.minecraft.network.protocol.common.custom.CustomPacketPayload;
 import net.minecraft.resources.Identifier;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public record WorldIdPayload(String worldName) implements CustomPacketPayload {
-    public static final CustomPacketPayload.Type<WorldIdPayload> PACKET_ID = new CustomPacketPayload.Type<>(Identifier.fromNamespaceAndPath(VoxelConstants.MOD_ID, "world_id"));
+    private static final Logger LOGGER = LogManager.getLogger("VoxelMap");
+    public static final CustomPacketPayload.Type<WorldIdPayload> PACKET_ID = new CustomPacketPayload.Type<>(Identifier.fromNamespaceAndPath("worldinfo", "world_id"));
     public static final StreamCodec<FriendlyByteBuf, WorldIdPayload> PACKET_CODEC = StreamCodec.ofMember(WorldIdPayload::encode, WorldIdPayload::decode);
 
     public static WorldIdPayload decode(FriendlyByteBuf buf) {
-        buf.readByte();
-        return new WorldIdPayload(buf.readUtf());
+        buf.readByte(); // ignore
+        int length;
+        int b = buf.readUnsignedByte();
+        if (b == 42) {
+            // "new" packet
+            length = buf.readUnsignedByte();
+        } else if (b == 0) {
+            // length == 0 ?
+            LOGGER.warn("Received unknown world_id packet");
+            return null;
+        } else {
+            // probably "legacy" packet
+            LOGGER.warn("Assuming legacy world_id packet. " +
+                    "The support might be removed in the future versions.");
+            length = b;
+        }
+        byte[] bytes = new byte[length];
+        buf.readBytes(bytes);
+        String worldName = new String(bytes, StandardCharsets.UTF_8);
+        return new WorldIdPayload(worldName);
     }
 
     public void encode(FriendlyByteBuf buf) {
         buf.writeByte(0);
-        buf.writeUtf(worldName);
+        buf.writeByte(42);
+        buf.writeByte(0);
     }
 
     @Override
