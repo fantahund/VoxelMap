304ad8363bc0d76ee3f86e0c972cfb829472e7a0
Brokkonaut <hannos17@gmx.de>
2026-07-17 03:19:17 +0200

    fix move scoreboard below map

--- common/src/main/java/com/mamiyaotaru/voxelmap/mixins/MixinInGameHud.java
+++ common/src/main/java/com/mamiyaotaru/voxelmap/mixins/MixinInGameHud.java
@@ -1,9 +1,13 @@
 package com.mamiyaotaru.voxelmap.mixins;
 
-import net.minecraft.client.gui.Gui;
+import com.llamalad7.mixinextras.sugar.Local;
+import com.mamiyaotaru.voxelmap.VoxelConstants;
+import net.minecraft.client.gui.Hud;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
-@Mixin(Gui.class)
+@Mixin(Hud.class)
 public class MixinInGameHud {
 
     // this method: private void displayScoreboardSidebar(GuiGraphics guiGraphics, Objective objective)
@@ -11,9 +15,10 @@ public class MixinInGameHud {
     //
     // entriesHeight is: int n = m * 9;
 
-//    TODO 26.1: fix scoreboard
-//    @ModifyVariable(method = "displayScoreboardSidebar(Lnet/minecraft/client/gui/GuiGraphics;Lnet/minecraft/world/scores/Objective;)V", at = @At("STORE"), ordinal = 5)
-//    private int injected(int bottomX, @Local(ordinal = 5) int entriesHeight) {
-//        return VoxelConstants.moveScoreboard(bottomX, entriesHeight);
-//    }
+    // at = bottom
+    // local entriesHeight = height
+    @ModifyVariable(method = "displayScoreboardSidebar(Lnet/minecraft/client/gui/GuiGraphicsExtractor;Lnet/minecraft/world/scores/Objective;)V", at = @At("STORE"), ordinal = 6)
+    private int injected(int bottomX, @Local(ordinal = 5) int entriesHeight) {
+        return VoxelConstants.moveScoreboard(bottomX, entriesHeight);
+    }
 }
