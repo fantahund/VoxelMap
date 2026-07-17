b505ee54fe87b11fdb8e897b907e1050120bb984
Gamja <algamja2011@gmail.com>
2026-07-17 17:21:57 +0900

    Add projection util

---
+++ common/src/main/java/com/mamiyaotaru/voxelmap/rendering/CachedProjectionMatrixBuffer.java
@@ -0,0 +1,40 @@
+package com.mamiyaotaru.voxelmap.rendering;
+
+import com.mojang.blaze3d.buffers.GpuBufferSlice;
+import net.minecraft.client.renderer.Projection;
+import net.minecraft.client.renderer.ProjectionMatrixBuffer;
+
+public class CachedProjectionMatrixBuffer {
+    public static CachedProjectionMatrixBuffer perspective(String name, float zNear, float zFar, float fov, float width, float height) {
+        Projection projection = new Projection();
+        projection.setupPerspective(zNear, zFar, fov, width, height);
+
+        return new CachedProjectionMatrixBuffer(name, projection);
+    }
+
+    public static CachedProjectionMatrixBuffer orthographic(String name, float zNear, float zFar, float width, float height, boolean invertY) {
+        Projection projection = new Projection();
+        projection.setupOrtho(zNear, zFar, width, height, invertY);
+
+        return new CachedProjectionMatrixBuffer(name, projection);
+    }
+
+    private final Projection projection;
+    private final ProjectionMatrixBuffer buffer;
+
+    public CachedProjectionMatrixBuffer(String name, Projection projection) {
+        this.projection = projection;
+        buffer = new ProjectionMatrixBuffer(name);
+    }
+
+    public GpuBufferSlice getBuffer() {
+        return buffer.getBuffer(projection);
+    }
+
+    public GpuBufferSlice getBuffer(float width, float height) {
+        if (projection.width() != width || projection.height() != height) {
+            projection.setSize(width, height);
+        }
+        return buffer.getBuffer(projection);
+    }
+}
