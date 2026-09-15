6b77266dc7037ba50d1f7c1566f31ee18f4ccf82
fantahund <tobi@emrion.de>
2026-09-15 10:47:16 +0200

    bump version

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ val fabricVersion by extra { "0.19.5" }
 val fabricApiVersion by extra { "0.160.4+26.3" }
 val modMenuVersion by extra { "20.0.1" }
 val paperApiVersion by extra { "[26.2.build,)" }
-val voxelMapVersion by extra { "1.16.9" }
+val voxelMapVersion by extra { "1.16.11" }
 val voxelConfigVersion by extra { "1.0.0" }
 
 val fullVersion by extra { "${minecraftVersion}-${voxelMapVersion}" }
